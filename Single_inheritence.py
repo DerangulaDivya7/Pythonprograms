b9e66cc0eb6ed7865913952f2ca27750773d87c9
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-04-27 11:16:01 +0530

    Create Single_inheritence.py

---
+++ Single_inheritence.py
@@ -0,0 +1,10 @@
+class father:
+    def car(self):
+        print("parent property...")
+class child(father):
+    def mobile (self):
+        print("child property ....")
+obj =child()
+obj.mobile()
+obj.car()
+              
