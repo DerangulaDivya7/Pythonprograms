b9f2d67da85c70eb37ccff10b8b9a091091858ee
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-07-21 22:31:10 +0530

    Create greatest_number.py

---
+++ greatest_number.py
@@ -0,0 +1,9 @@
+a=int(input("enter value"))
+b=int(input("enter value"))
+c=int(input("enter value"))
+if a>b and a>c:
+    print(a,"is greater")
+elif b > c and b > a:
+        print(b, "is greater")
+else:
+    print(c,"c is greater")
