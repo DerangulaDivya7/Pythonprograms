34fb54618708161136bd9cb3920e8f08b2e297ab
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-07-28 21:49:25 +0530

    Create sll-insertbegin.py

---
+++ sll-insertbegin.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class sLL():
+    def __init__(self):
+     self.head=None
+    def insert_begin(self,data):
+        nb=Node(data)
+        nb.next=self.head
+        self.head=nb
+    def display(self):
+        if self.head is None:
+            print("single linked list is empty")
+        else:
+           temp= self.head
+           while temp:
+               print(temp.data,"-->",end="")
+               temp=temp.next
+l=sLL()
+n=Node(10)
+l.head=n
+n1=Node(20)
+n.next=n1
+n2=Node(30)
+n1.next=n2
+n3=Node(40)
+n2.next=n3
+n4=Node(50)
+n3.next=n4
+l.insert_begin(90)
+l.display()
