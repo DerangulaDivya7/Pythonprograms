108a68d3bca07696cd84b92862c8b420334087f1
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-07-29 21:31:32 +0530

    Create sll_insert_atposition.py

---
+++ sll_insert_atposition.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class sLL():
+    def __init__(self):
+     self.head=None
+    def insert_begin(self,data):
+        nb=Node(data)
+        nb.next=self.head
+        self.head=nb
+    def insert_end(self,data):
+        ne=Node(data)
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=ne
+    def insert_position(self,data,pos):
+        np=Node(data)
+        temp=self.head
+        for i in range(pos-1):
+            temp=temp.next
+        np.data=data
+        np.next=temp.next
+        temp.next=np
+
+    def display(self):
+        if self.head is None:
+            print("single linked list is empty")
+        else:
+           temp= self.head
+           while temp:
+               print(temp.data,"-->",end="")
+               temp=temp.next
+l=sLL()
+n=Node(10)
+l.head=n
+n1=Node(20)
+n.next=n1
+n2=Node(30)
+n1.next=n2
+n3=Node(40)
+n2.next=n3
+n4=Node(50)
+n3.next=n4
+l.insert_begin(90)
+l.insert_end(60)
+l.insert_end(100)
+l.insert_position(45,4)
+l.display()
