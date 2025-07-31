acd74f1f59c5c0f678839e32f52009c54f672e64
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-07-31 19:09:19 +0530

    Create del_beg.py

---
+++ del_beg.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self,data):
+      self.data=data
+      self.next=None
+class sll:
+   def __init__(self):
+      self.head=None
+   def display(self):
+      if self.head is None:
+         print("empty")
+      else:
+         temp=self.head
+         while temp:
+            print(temp.data,"--->",end=" ")
+            temp=temp.next
+   def del_begg(self):
+      temp =self.head
+      self.head=temp.next
+      temp.next=None
+l=sll()
+n1=Node(10)
+l.head=n1
+n2=Node(20)
+n1.next=n2
+n3=Node(30)
+n2.next=n3
+n4=Node(40)
+n3.next=n4
+l.del_begg()
+l.display()
