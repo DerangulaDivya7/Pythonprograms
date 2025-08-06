3018068eb846fd0d4858c752c77cca56ea1c0eed
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-08-06 22:18:35 +0530

    Create searchlist.py

---
+++ searchlist.py
@@ -0,0 +1,7 @@
+fruits=['apple','orange','mango','mango','guvva']
+search=input("enter the fruit name")
+if search in fruits:
+    print("yes",search,"in the list")
+else:
+    print("no",search,'in the list')
+    
