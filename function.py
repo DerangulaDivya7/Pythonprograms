fb6476f509a0030e470e86ec396dc9136ecf0393
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-08-15 22:24:43 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,6 @@
+def describe_pet(animal_type, pet_name):
+  print(f"\nI have a {animal_type} named {pet_name}.")
+describe_pet('hamster', 'Harry')
+describe_pet('dog', 'Lucy')
+describe_pet(animal_type='cat', pet_name='Whiskers')
+describe_pet(pet_name='Buddy', animal_type='goldfish')
